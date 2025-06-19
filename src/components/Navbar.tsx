2a002a953624ca9a09df888132fa1da79aada039
boyka <abdelrahmanzaki747@gmail.com>
2025-06-19 15:15:48 +0300

    add some comments & design edits

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,30 +5,37 @@ import TopBar from "./TopBar";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+      {/* Logo and Title */}
       <div className="flex items-center gap-4">
         <Link href={"/"} className="flex gap-1 items-center">
           <span className="text-2xl font-bold bg-red-500 py-1 px-2 rounded-lg">
             IMDb
           </span>
-          <span className="text-xl hidden sm:inline">Clone</span>
+          <span className="text-xl hidden sm:inline">Clone </span>
         </Link>
       </div>
+
+      {/* Trend & Top Rated Links  */}
       <div className="flex items-center">
         <TopBar />
       </div>
-      <ul className="flex gap-4">
-        <li className="hidden sm:block">
+
+      {/* Navigation Links */}
+      <ul className="flex gap-4 text-xl ">
+        <li className="hidden sm:block hover:text-red-500">
           <Link href={"/"}>Home</Link>
         </li>
-        <li className="hidden sm:block">
+        <li className="hidden sm:block hover:text-red-500">
           <Link href={"/favorites"}>Favorites</Link>
         </li>
-        <li className="hidden sm:block">
+        <li className="hidden sm:block hover:text-red-500">
           <Link href={"/about"}>About</Link>
         </li>
+
+        {/* Authentication Buttons */}
         <SignedOut>
           <SignInButton mode="modal">
-            <button className=" cursor-pointer   ">
+            <button className=" cursor-pointer   hover:text-red-500">
               SignIn
             </button>
           </SignInButton>
--- src/components/TopItems.tsx
+++ src/components/TopItems.tsx
@@ -8,6 +8,9 @@ type Props = {
 };
 
 const TopItems = ({ title, param }: Props) => {
+  // Generate a link to the top items page (/top/trending) using the given param and title;
+  // highlight it if it's the active route based on the current pathname
+
   const genre = usePathname().split("/")[2];
   return (
     <div>
