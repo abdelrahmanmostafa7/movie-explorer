810963f35e313c282d8bcc5033e285a2a30d1aca
boyka <abdelrahmanzaki747@gmail.com>
2025-06-20 11:10:16 +0300

    Refactor Home component to include title and description for better UI & UX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,19 @@ type Props = {
 export default async function Home({ params }: Props) {
   try {
     const results = await fetchMovies(params.genre, API_KEY);
-    return <Results results={results} />;
+
+    return (
+      <>
+        <h1 className="text-3xl font-bold text-center my-6">
+          Hot New Releases: Must-See Movies & Shows for 2024–2025
+        </h1>
+        <p className="text-lg text-center text-gray-400 max-w-3xl mx-auto px-4 mb-8">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, repellat! Iusto, architecto ex suscipit quisquam voluptates debitis vero nulla accusantium veniam accusamus temporibus officiis rem pariatur earum repellendus non eum. 🎬✨
+        </p>
+
+        <Results results={results} />
+      </>
+    );
   } catch (error) {
     console.error("Error fetching movie data", error);
     throw new Error("Failed to fetch movie data");
